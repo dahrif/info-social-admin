29d997b0ca66b19f4ddec5a2cf29f7cf545a9470
Fatima Dahri <dahri.fatima.dev@gmail.com>
2022-07-28 14:41:57 +0100

    save changes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,46 +5,49 @@ import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   isLoggedInGuard: boolean = false;
 
-  constructor(private afAuth: AngularFireAuth, private toastr: ToastrService, private router: Router) { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
 
-  login(email:any, password:any){
-    this.afAuth.signInWithEmailAndPassword(email, password).then(logRef =>{
-      this.toastr.success('Logged in Successfully');
-      this.loadUser();
-      this.loggedIn.next(true);
-      this.isLoggedInGuard = true;
-      this.router.navigate(['/']);
-    }).catch(e =>{
-      this.toastr.warning("email or password incorrect");
-    })
+  login(email: any, password: any) {
+    this.afAuth
+      .signInWithEmailAndPassword(email, password)
+      .then((logRef) => {
+        this.toastr.success('Logged in Successfully');
+        this.loadUser();
+        this.loggedIn.next(true);
+        this.isLoggedInGuard = true;
+        this.router.navigate(['/']);
+      })
+      .catch((e) => {
+        this.toastr.warning('email or password incorrect');
+      });
   }
 
-  loadUser(){
-    this.afAuth.authState.subscribe(user =>{
-      localStorage.setItem('user', JSON.stringify(user));    
+  loadUser() {
+    this.afAuth.authState.subscribe((user) => {
+      localStorage.setItem('user', JSON.stringify(user));
     });
   }
 
-
-  logOut(){
-    this.afAuth.signOut().then(()=>{
+  logOut() {
+    this.afAuth.signOut().then(() => {
       localStorage.removeItem('user');
       this.loggedIn.next(false);
       this.isLoggedInGuard = false;
-    this.router.navigate(['/login']);
+      this.router.navigate(['/login']);
     });
   }
 
-  isLoggedIn(){
+  isLoggedIn() {
     return this.loggedIn.asObservable();
   }
-
-
 }
